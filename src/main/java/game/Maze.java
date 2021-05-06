bd5edd6e7820aafdef469c98408e65e687032c62
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2021-05-06 19:38:13 +0200

    Make sure maze is valid

--- src/main/java/game/Maze.java
+++ src/main/java/game/Maze.java
@@ -34,7 +34,7 @@ public class Maze {
             cells.add(cellsHelper);
         }
 
-        System.out.println(cells);
+        validate();
 
         addPositions();
     }
@@ -43,5 +43,53 @@ public class Maze {
         cells.get(start.getY()).set(start.getX(), (byte) (cells.get(start.getY()).get(start.getX()) | 0b10000));
 
         cells.get(finish.getY()).set(finish.getX(), (byte) (cells.get(finish.getY()).get(finish.getX()) | 0b100000));
+    }
+
+    private void validate() throws AssertionError {
+        for (int i = 0; i < cells.size(); i++) {
+            for (int j = 0; j < cells.get(i).size(); j++) {
+                ensureSurroundingWalls(i, j);
+
+                ensureDoubleWalls(i, j);
+
+                if (cells.get(i).get(j) > 15) {
+                    throw new AssertionError("Invalid cell at: " + "x: " + j + " y: " + i);
+                }
+            }
+        }
+    }
+
+    private void ensureSurroundingWalls(int row, int col) {
+        if (row == 0) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | 0b1));
+        }
+
+        if (row == cells.size() - 1) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | 0b100));
+        }
+
+        if (col == 0) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | 0b1000));
+        }
+
+        if (col == cells.get(row).size() - 1) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | 0b10));
+        }
+    }
+
+    private void ensureDoubleWalls(int row, int col) {
+        if (cells.size() - 1 > row) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | ((0b1 & cells.get(row + 1).get(col)) << 2)));
+        }
+        if (row != 0) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | ((0b100 & cells.get(row - 1).get(col)) >> 2)));
+        }
+
+        if (cells.get(row).size() - 1 > col) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | ((0b1000 & cells.get(row).get(col + 1)) >> 2)));
+        }
+        if (col != 0) {
+            cells.get(row).set(col, (byte) (cells.get(row).get(col) | ((0b10 & cells.get(row).get(col - 1)) << 2)));
+        }
     }
 }
